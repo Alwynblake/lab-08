78b52cf9acd456f76d5b648308e606f79600c676
Clarice <claricostello@gmail.com>
2018-12-06 09:36:06 -0800

    created schema.sql

---
+++ schema.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS weathers;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
+    latitude NUMERIC(8, 6),
+    longitute NUMERIC(9, 6)
+);
+
+CREATE TABLE weathers (
+    id SERIAL PRIMARY KEY,
+    forecase VARCHAR(255),
+    time VARCHAR(255),
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    image_url VARCHAR(255),
+    price VARCHAR(255),
+    rating VARCHAR(255),
+    url VARCHAR(255),
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    overview VARCHAR(255),
+    average_votes VARCHAR(255),
+    total_votes VARCHAR(255),
+    image_url VARCHAR(255),
+    popularity VARCHAR(255),
+    released_on VARCHAR(255),
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
